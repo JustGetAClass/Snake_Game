93edc21f56edbcd09742a43867f67960c3affac1
Mohamed Salat <89856630+JustGetAClass@users.noreply.github.com>
2022-11-24 12:11:11 +0300

    high score is now saved

--- scoreboard.py
+++ scoreboard.py
@@ -11,7 +11,8 @@ class Scoreboard(Turtle):
         self.color("white")
         self.penup()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as file:
+            self.high_score = int(file.read())  # read the high score from data.txt
         self.hideturtle()
         self.goto(0, 270)
         self.update_score()
@@ -23,6 +24,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.high_score))  # write the new high score into data.txt
         self.score = 0
         self.update_score()
 
