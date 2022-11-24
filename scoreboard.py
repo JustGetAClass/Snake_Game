eb229e98fd8b45c733f66c02403116e2018c63b9
Mohamed Salat <89856630+JustGetAClass@users.noreply.github.com>
2022-11-24 11:16:18 +0300

    added high score

--- scoreboard.py
+++ scoreboard.py
@@ -11,18 +11,25 @@ class Scoreboard(Turtle):
         self.color("white")
         self.penup()
         self.score = 0
+        self.high_score = 0
         self.hideturtle()
         self.goto(0, 270)
         self.update_score()
 
     def update_score(self):
-        self.write(f"score : {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score : {self.score}  High Score : {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_score()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
     def add_score(self):
-        self.clear()
         self.score += 1
         self.update_score()
